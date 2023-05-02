3e1af6ad748d4d3ee2a6afba0de2144e5e756c47
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-02 12:34:55 +0200

    Fix imports stimulus controllers from components.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,8 @@
 
 import { application } from "./application"
 
+import ExampleComponentController from "./../../components/example_component_controller"
+application.register("example-component", ExampleComponentController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
