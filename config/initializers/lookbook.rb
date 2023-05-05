0c4693b9263d159d313da117101d025644f1666a
Mateusz Bialowas <58574619+mateuszbialowas@users.noreply.github.com>
2023-05-05 23:44:34 +0200

    Config/deploy (#5)
    * deploy
    
    * Revert "deploy"
    
    This reverts commit 64825c029212e2327a2cfaf524451ecae497bb79.
    
    * Config Procfile.
    
    * Do not set lookbook config in other environments.

--- config/initializers/lookbook.rb
+++ config/initializers/lookbook.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+return unless Rails.env.development?
+
 Rails.application.configure do
   config.lookbook.project_name = 'OgloRadar'
   config.lookbook.ui_theme = 'blue'
