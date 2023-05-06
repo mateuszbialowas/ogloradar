7b3e5fd22dd688d06e8f663784d853d53a3c1ac4
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-06 19:00:31 +0200

    Config flashes

--- config/initializers/lookbook.rb
+++ config/initializers/lookbook.rb
@@ -5,5 +5,5 @@ return unless Rails.env.development?
 Rails.application.configure do
   config.lookbook.project_name = 'OgloRadar'
   config.lookbook.ui_theme = 'blue'
-  config.lookbook.preview_layout = "component_preview"
+  config.lookbook.preview_layout = 'component_preview'
 end
