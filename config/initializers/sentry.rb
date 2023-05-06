df12da476dac890d7418f7b40a920a181c4d5ece
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-07 01:38:20 +0200

    config sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,7 +4,7 @@ return unless Rails.env.staging?
 
 require 'sentry-rails'
 Sentry.init do |config|
-  config.dsn = 'https://20591b4f23b04f8b9ddebe4e8fdf2bab@o4503994140655616.ingest.sentry.io/4505139644334080'
+  config.dsn = Rails.credentials.dig(:sentry, Rails.env.to_sym, :dsn)
   config.breadcrumbs_logger = %i[active_support_logger http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
