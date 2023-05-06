62324c200c4cfaca91824b3fe12544c0fdc1eb1c
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-06 19:47:28 +0200

    Config flashes

--- app/components/flash_component_controller.js
+++ app/components/flash_component_controller.js
@@ -2,9 +2,19 @@ import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller {
     connect() {
+        this.element.classList.add('animate__animated', 'animate__fadeInRight', 'animate__faster');
     }
 
     close() {
+        this.element.classList.remove('animate__fadeInRight');
+        this.element.classList.add('animate__fadeOutRight');
+
+        this.element.addEventListener('animationend', () => {
+            this.element.remove();
+        });
+    }
+
+    disconnect() {
         this.element.remove();
     }
 }
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,9 @@
 
 class PagesController < ApplicationController
   def home
-    flash[:info] = 'This is an info message.'
-    flash[:success] = 'This is a success message.'
-    flash[:error] = 'This is an error message.'
+    flash.now[:info] = 'This is an info message.'
+    flash.now[:success] = 'This is a success message.'
+    flash.now[:error] = 'This is an error message.'
     flash.now[:alert] = 'This is an alert message.'
   end
 end
