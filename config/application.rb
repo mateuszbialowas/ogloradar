935662132c51fceff3a319102e8b13d6467f195c
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-06 12:14:19 +0200

    Config translations

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,11 @@ module Ogloradar
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = "Warsaw"
+    config.active_record.default_timezone = :local
+
+    config.i18n.default_locale = :pl
+    I18n.available_locales = [:pl]
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
