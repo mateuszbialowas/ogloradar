a9de82646ac03e557056c12d1ac1ae704cf379ff
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-06 19:27:16 +0200

    Config flashes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
-  def home; end
+  def home
+    flash[:info] = 'This is an info message.'
+    flash[:success] = 'This is a success message.'
+    flash[:error] = 'This is an error message.'
+    flash.now[:alert] = 'This is an alert message.'
+  end
 end
