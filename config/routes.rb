23b8284db98ec5fabde5db53ee0cf8816edc37cc
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-18 01:26:32 +0200

    .

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,9 @@ require 'sidekiq-scheduler/web'
 Rails.application.routes.draw do
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
+    mount Lookbook::Engine, at: '/lookbook' if Rails.env.development?
   end
-  mount Lookbook::Engine, at: '/lookbook' if Rails.env.development?
 
   devise_for :users, controllers: { registrations: 'users/registrations' }
   root 'dashboard#index'
-
-  authenticated :user do
-    resources :searches
-  end
 end
