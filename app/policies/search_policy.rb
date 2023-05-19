53de747dfb3a8ea8f15fce4d08349b1c0ca9ff91
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-05-20 00:31:42 +0200

    Destroy service

--- app/policies/search_policy.rb
+++ app/policies/search_policy.rb
@@ -1,35 +1,6 @@
 # frozen_string_literal: true
 
 class SearchPolicy < ApplicationPolicy
-
-  def index?
-    true
-  end
-
-  def show?
-    true
-  end
-
-  def create?
-    true
-  end
-
-  def new?
-    create?
-  end
-
-  def update?
-    true
-  end
-
-  def edit?
-    update?
-  end
-
-  def destroy?
-    true
-  end
-
   class Scope < Scope
     def resolve
       scope.where(user:)
