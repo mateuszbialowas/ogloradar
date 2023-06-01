4313bd476de2f392fcf65d05952c4d82889b64cc
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-06-02 00:15:38 +0200

    Update config.asset_host

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -33,7 +33,7 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.asset_host = 'https://ogloradar-stg.mateuszbialowas.com'
+  config.asset_host = 'https://ogloradar.mateuszbialowas.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
