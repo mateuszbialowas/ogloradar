7c4733aeeeef2ef0b7cb927d639c556a79032349
Mateusz Bialowas <58574619+mateuszbialowas@users.noreply.github.com>
2023-06-04 19:57:43 +0200

    Implement custom errors pages. (#26)
    * Implement custom errors pages.
    
    * Implement custom errors pages.
    
    * Rubocop -A. Erblint. Fix test.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@
 require 'sidekiq/web'
 require 'sidekiq-scheduler/web'
 Rails.application.routes.draw do
+  match '/404', to: 'errors#not_found', via: :all
+  match '/422', to: 'errors#unprocessable_entity', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
   authenticate :user, ->(user) { user.admin? } do
     mount Sidekiq::Web => '/sidekiq'
     mount Lookbook::Engine, at: '/lookbook' if Rails.env.development?
