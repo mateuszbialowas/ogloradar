3f2fd106de732d7820d24b9af97e84a88bb4e7f9
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-06-09 21:08:25 +0200

    Rubocop -A

--- app/avo/resources/search_resource.rb
+++ app/avo/resources/search_resource.rb
@@ -10,7 +10,7 @@ class SearchResource < Avo::BaseResource
   field :id, as: :id
   field :name, as: :text
   field :uri, as: :text, only_on: :show
-  field :status, as: :select, enum: ::Search.statuses, hide_on: [:show, :index], placeholder: 'Choose the status.'
+  field :status, as: :select, enum: ::Search.statuses, hide_on: %i[show index], placeholder: 'Choose the status.'
   field :status, as: :badge, options: { success: 'active', danger: 'inactive' }
   field :user, as: :belongs_to
   field :products_count, as: :number do |search|
