ca537f95318ce4e6ec0e96cf4fcac0f2d0f7ef0e
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-06-18 22:30:20 +0200

    Style products index

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,8 @@
 class Product < ApplicationRecord
   belongs_to :search
   enum external_service_name: { olx: 'olx' }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at]
+  end
 end
