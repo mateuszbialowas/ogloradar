a3e3d812816c08922a0afcaa4c416d80d4612546
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-06-18 19:42:05 +0200

    .

--- test/components/previews/forms/password_field_component_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Forms
-  class PasswordFieldComponentPreview < ViewComponent::Preview
-    def default
-      render(Forms::PasswordFieldComponent.new(form: 'form', method: 'method', placeholder: 'placeholder'))
-    end
-  end
-end
