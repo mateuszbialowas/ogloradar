09d90480e26991f72af1a266be4239c5adf3d887
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-06-18 16:56:41 +0200

    wip config

--- config/initializers/simple_form.rb
+++ config/initializers/simple_form.rb
@@ -59,7 +59,8 @@ SimpleForm.setup do |config|
     end
   end
 
-  config.button_class = 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'
+  config.button_class = 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300
+                         font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center'
 
   # Method used to tidy up errors. Specify any Rails Array method.
   # :first lists the first message for each field.
