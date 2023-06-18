b05a11d93dd8e697ef4093767a88b462c9083e02
Mateusz Bialowas <58574619+mateuszbialowas@users.noreply.github.com>
2023-06-18 23:24:41 +0200

    Add search navbar. (#46)
    * Add search navbar.
    
    * Style products index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class ProductsController < AuthenticatedController
-  def index
-    pagy, products = pagy(Search.find(params[:search_id]).products)
-    render 'products/index', locals: { products:, pagy: }
+  before_action :search, only: %i[index]
+
+  def index # rubocop:disable Metrics/AbcSize
+    add_breadcrumb "Wyszukiwanie: #{search.name}", search_path(search)
+    add_breadcrumb 'Produkty'
+    q = search.products.ransack(params[:q])
+    q.sorts = 'created_at desc' if q.sorts.empty?
+    pagy, products = pagy(q.result(distinct: true))
+    render 'products/index', locals: { products:, search:, pagy:, q: }
+  end
+
+  private
+
+  def search
+    @search ||= policy_scope(Search).find(params[:search_id])
   end
 end
