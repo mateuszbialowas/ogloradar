aabe4715fb551c1e55a8a5c01961eb8cb70df792
mateuszbialowas <mateusz.bialowas2@gmail.com>
2023-06-19 22:32:45 +0200

    Config slim select input.

--- app/form_builders/custom_inputs/slim_select_input.rb
+++ app/form_builders/custom_inputs/slim_select_input.rb
@@ -5,10 +5,8 @@ module CustomInputs
     def input(wrapper_options = nil)
       label_method, value_method = detect_collection_methods
 
-      # merged_input_options = merge_wrapper_options(input_html_options.merge(data: { controller: 'slim-select-input' }),
-      #                                              wrapper_options)
-      merged_input_options = merge_wrapper_options({ class: nil, data: { controller: 'slim-select-input' } }, wrapper_options.except(:class))
-
+      merged_input_options = merge_wrapper_options(input_html_options.merge(data: { controller: 'slim-select-input' }),
+                                                   wrapper_options)
 
       @builder.collection_select(
         attribute_name, collection, value_method, label_method,
